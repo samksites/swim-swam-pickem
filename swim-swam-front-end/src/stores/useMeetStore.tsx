8bc9817ae6ec22cc43f58a8fbde1d71e2cf84e23
Samksites <Samuelksites@gmail.com>
2025-07-20 22:56:49 -0400

    create meet data store

---
+++ swim-swam-front-end/src/stores/useMeetStore.tsx
@@ -0,0 +1,56 @@
+// useMeetStore.ts
+import { create } from "zustand";
+import exampleSwimMeet from "../data/exampleSwimMeet.json";
+
+type Swimmer = {
+  name: string;
+  time: string;
+  // add other swimmer properties as needed
+};
+
+type Event = {
+  title: string;
+  swimmers: Swimmer[];
+  // add other event properties as needed
+};
+
+type Day = {
+  events: Event[];
+  title: string;
+  // add other day properties as needed
+};
+
+type MeetData = {
+  id: string;
+  days: Day[];
+  entriesCloseDate: string;
+  startDate: string;
+  title: string;
+  status: number;
+  gender: string;
+  type: string;
+  seedTimes: boolean;
+  allEvents: string[];
+  // add other meet properties as needed
+};
+
+type MeetStore = {
+    meetData: typeof exampleSwimMeet;
+    setMeetData: (data: typeof exampleSwimMeet) => void;
+    updateTitle: (
+        title: string,
+    ) => void;
+};
+
+export const useMeetStore = create<MeetStore>((set) => ({
+    meetData: {} as MeetData, // Initialize with an empty object
+
+    setMeetData: (data) => set({ meetData: data }),
+
+    updateTitle: (title: string) =>
+        set((state) => {
+            const updatedMeet = structuredClone(state.meetData);
+            updatedMeet.title = title;
+            return { meetData: updatedMeet };
+        }),
+}));
